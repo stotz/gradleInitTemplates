7b36eeaa17c9dea68b8a64bac676206eab7fb5cb
Urs Stotz <ursstotz@gmail.com>
2025-11-15 21:46:01 +0100

    Update ktor template: use newer versions (3.3.2), remove application plugin, add missing dependencies

--- ktor/build.gradle.kts
+++ ktor/build.gradle.kts
@@ -1,35 +1,22 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
-    application
 }
 
 group = "{{ group }}"
 version = "{{ version }}"
 
-repositories {
-    mavenCentral()
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
 }
 
 dependencies {
-    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.core.jvm)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.config.yaml)
     
     testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test)
-}
-
-kotlin {
-    jvmToolchain({{ jdk_version }})
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of({{ jdk_version }})
-    }
-}
-
-application {
-    mainClass.set("{{ group }}.ApplicationKt")
+    testImplementation(libs.kotlin.test.junit)
 }
