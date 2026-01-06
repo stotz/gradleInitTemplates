83d611b9b13d2a00074f24251c94ed18abc57f46
Urs Stotz <ursstotz@gmail.com>
2026-01-07 00:01:19 +0100

    Refactor repository configuration in `buildSrc`: move repositories block to `settings.gradle.kts` and update dependency management settings.

--- kotlin-multi/buildSrc/build.gradle.kts
+++ kotlin-multi/buildSrc/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:{{ kotlin_version }}")
 }
---
+++ kotlin-multi/buildSrc/settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "buildSrc"
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
