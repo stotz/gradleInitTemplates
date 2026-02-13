191a508c82ba8d191594fc29bb905e78dc58260e
Urs Stotz <ursstotz@gmail.com>
2026-02-13 12:47:03 +0100

    0003 Refactor: Replace `configureRouting` with `module` and remove embedded server initialization

--- ktor/src/main/kotlin/Application.kt
+++ ktor/src/main/kotlin/Application.kt
@@ -1,24 +1,16 @@
 package {{ group }}
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.serialization.Serializable
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-    }.start(wait = true)
-}
-
 @Serializable
 data class Message(val message: String, val application: String)
 
-fun Application.configureRouting() {
+fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
