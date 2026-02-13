16258eb3342cd00acbb6742dbaa700e37e194b68
Urs Stotz <ursstotz@gmail.com>
2026-02-13 21:00:17 +0100

    0004 Remove unused versionCatalogs configuration from settings.gradle.kts

--- kotlin-multi/settings.gradle.kts
+++ kotlin-multi/settings.gradle.kts
@@ -5,12 +5,6 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
-    
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
 }
 
 include("app")
